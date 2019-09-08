70973479e4e6617a607ad50a62c3561d2d3155aa
Jonathan Taylor <jonyonson@icloud.com>
2019-09-08 15:29:38 -0500

    MVP complete

--- src/App.js
+++ src/App.js
@@ -6,37 +6,28 @@ import data from './data';
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+import ProductContext from './contexts/ProductContext';
+import CardContext from './contexts/CartContext';
 
 function App() {
-	const [products] = useState(data);
-	const [cart, setCart] = useState([]);
+  const [products] = useState(data);
+  const [cart, setCart] = useState([]);
 
-	const addItem = item => {
-		setCart([...cart, item]);
-	};
+  const addItem = item => {
+    setCart([...cart, item]);
+  };
 
-	return (
-		<div className="App">
-			<Navigation cart={cart} />
-
-			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
-
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
-		</div>
-	);
+  return (
+    <ProductContext.Provider value={{ products, addItem }}>
+      <CardContext.Provider value={{ cart, setCart }}>
+        <div className="App">
+          <Navigation />
+          <Route exact path="/" component={Products} />
+          <Route path="/cart" component={ShoppingCart} />
+        </div>
+      </CardContext.Provider>
+    </ProductContext.Provider>
+  );
 }
 
 export default App;
